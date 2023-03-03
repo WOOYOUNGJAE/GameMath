9558783efb04093f7b181b9cf8605fadc5c57cc2
WOOYOUNGJAE <azn2ww@naver.com>
2023-03-03 19:50:14 +0900

    Update SoftRenderer2D.cpp

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -67,6 +67,7 @@ void SoftRenderer::Update2D(float InDeltaSeconds)
 
 	float deltaDegree = input.GetAxis(InputAxis::WAxis) * rotateSpeed * InDeltaSeconds;
 
+
 	// 물체의 최종 상태 설정
 	currentDegree += deltaDegree;
 }
@@ -82,30 +83,61 @@ void SoftRenderer::Render2D()
 	DrawGizmo2D();
 
 	// 렌더링 로직의 로컬 변수
-	static float halfSize = 100.f;
-	static std::vector<Vector2> squares;
-
+	static float halfSize = 100.f; // 정사각형의 절반 크기
+	static std::vector<Vector2> squares; squares.reserve(160000);
+	
 	// 사각형을 구성하는 점을 생성
 	if (squares.empty())
 	{
 		for (float x = -halfSize; x <= halfSize; x += 0.25f)
-		{
-			for (float y = -halfSize; y <= halfSize; y += 0.25f)
+		{		for (float y = -halfSize; y <= halfSize; y += 0.25f)
 			{
 				squares.push_back(Vector2(x, y));
 			}
 		}
 	}
 
+	// 각도에 해당하는 sin, cos 구하기
+	float sin = 0.f, cos = 0.f;
+	Math::GetSinCos(sin, cos, currentDegree);
+
+	// 현재 화면의 크기로부터 길이를 비교할 기준양 정하기
+	static float maxLength = Vector2(_ScreenSize.X, _ScreenSize.Y).Size() * 0.5f; // 대각선의 길이의 반절
+
+
 	// 사각형 그리기
 	HSVColor hsv(0.f, 1.f, 0.85f);
 	for (auto const& v : squares)
 	{
-		r.DrawPoint(v, hsv.ToLinearColor());
-	}
+		// 극좌표계로 변경
+		Vector2 polarV = v.ToPolarCoordinate();
+		
+		// 극좌표계의 각 정보로부터 색상 결정
+		// [0, 2pi]로 변경하기 위해 2pi를 더해줌
+		if (polarV.Y < 0.f) // 극좌표계 정의역[-pi, +pi]에 해당하지 않다면
+		{
+			polarV.Y += Math::TwoPI;  // 음수면 되돌리기
+		}
+
+		hsv.H = polarV.Y / Math::TwoPI; // 극좌표계의 값을 [0,1]로 정규화하여 색상을 지정
+
+		// 극좌표계의 크기 정보로부터 회전량 결정
+		float ratio = polarV.X / maxLength; // 벡터사이즈/대각선길이반절, 중심에서 멀어질수록 커짐
+		// weight 없으면 일반 회전. weight를 곱함으로써 중심부-바깥 회전량 차이
+		float weight = Math::Lerp(1.f, 5.f, ratio); // 중심에서 가까우면 1에, 멀면 5에 가까움
+
+		// 극좌표계를 사용해 회전을 부여
+		// 회전량에 가중치를 곱해 최종 회전량 계산. 중심부에서 바깥으로 갈수록 많은 가중치
+		polarV.Y += Math::Deg2Rad(currentDegree) * weight;
+
+		// 최종 값을 데카르트 좌표계로 변환
+		Vector2 cartesianV = polarV.ToCartesianCoordinate();
+		r.DrawPoint(cartesianV, hsv.ToLinearColor());
+
+	} //전체 흐름: 점 폴라로, (음수면 보정),  각도 += 변화시킬각도 * 가중치
 
 	// 현재 각도를 화면에 출력
-	r.PushStatisticText(std::string("Degree : ") + std::to_string(currentDegree));
+	//r.PushStatisticText(std::string("Degree : ") + std::to_string(currentDegree));
 }
 
 // 메시를 그리는 함수
